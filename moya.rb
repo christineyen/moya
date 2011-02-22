08cc5348bedc3bcb2dd067c838e72b81b53f3673
Christine Yen <cyen@christineyen.com>
2011-02-22 04:04:04 -0800

    Items are pulled + added to Instapaper one by one!

--- moya.rb
+++ moya.rb
@@ -6,23 +6,54 @@ require 'net/https'
 require 'uri'
 require 'json'
 
-RIL_TEMPLATE = 'https://readitlaterlist.com/v2/get?username=USERNAME&password=PASSWORD&apikey=APIKEY&count=2'
+RIL_TEMPLATE = 'https://readitlaterlist.com/v2/get?username=USERNAME&password=PASSWORD&apikey=APIKEY&count=5'
+INST_TEMPLATE = 'https://www.instapaper.com/api/add'
 
 helpers do
   # Call the ReadItLater API
   def fetch_readitlater_items(username, password)
     ril_url = RIL_TEMPLATE.gsub(/APIKEY/, ENV['RIL_API_KEY']).
-        gsub(/USERNAME/, params['ril']['username']).
-        gsub(/PASSWORD/, params['ril']['password'])
+        gsub(/USERNAME/, username).
+        gsub(/PASSWORD/, password)
 
     uri = URI.parse(ril_url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
+    http.request(request)
+  end
 
-    JSON.parse(response.body)
+  # Items are passed in (via RIL) in the following format:
+  # { 'list' =>
+  #   { _id_1_ =>
+  #     { 'time_updated' => __,
+  #       'title' => __,
+  #       'time_added' => __,
+  #       'url' => __,
+  #       'item_id' => __,
+  #       'state' => __ }, # '0' is unread; '1' is read
+  #   { _id_2_ => .....
+  # We will return read items, in added order, to be sent to Instapaper:
+  #   url + title
+  def convert_ril_json_to_instapaper(ril_items)
+    items = ril_items['list'].values.sort_by{ |elt| elt['time_added'] }
+    items.map do |item|
+      { :url => item['url'], :title => item['title'] }
+    end
+  end
+
+  # Call the Instapaper API to create all the items
+  def create_instapaper_items(username, password, item)
+    uri = URI.parse(INST_TEMPLATE)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.basic_auth(username, password)
+    request.set_form_data({ 'title' => item[:title], 'url' => item[:url] })
+    http.request(request)
   end
 end
 
@@ -31,7 +62,20 @@ get '/' do
 end
 
 post '/migrate' do
-  raise json.inspect
+  ril_items = fetch_readitlater_items(
+      params['ril']['username'], params['ril']['password'])
+  raise "error! #{ril_items.code} received" unless ril_items.code == '200'
+
+  json = JSON.parse(ril_items.body)
+  inst_items = convert_ril_json_to_instapaper(json)
+
+  inst_username = params['inst']['username']
+  inst_password = params['inst']['password']
+  inst_items.each do |item|
+    resp = create_instapaper_items(inst_username, inst_password, item)
+  end
+
+  #raise ril_items.inspect
 
   haml :migrate
 end
