4ce0c2aba8f4b59b92504c136540e52419300a48
Christine Yen <cyen@christineyen.com>
2011-02-22 04:58:19 -0800

    Clean up error handling around auth / etc.

--- moya.rb
+++ moya.rb
@@ -6,23 +6,25 @@ require 'net/https'
 require 'uri'
 require 'json'
 
-RIL_TEMPLATE = 'https://readitlaterlist.com/v2/get?username=USERNAME&password=PASSWORD&apikey=APIKEY&count=5'
-INST_TEMPLATE = 'https://www.instapaper.com/api/add'
+RIL_GET = 'https://readitlaterlist.com/v2/get?username=USERNAME&password=PASSWORD&apikey=APIKEY&count=5'
+INST_ADD = 'https://www.instapaper.com/api/add'
+INST_AUTH = 'https://www.instapaper.com/api/authenticate'
+
+ERR_UNAUTHORIZED = 'Looks like you provided incorrect credentials! Try again?'
+ERR_REMOTE = "Not our fault! Looks like something's up remotely. Try again later."
+ERR_VAGUE = "Something mysterious and bad happened. We're on it!"
 
 helpers do
   # Call the ReadItLater API
   def fetch_readitlater_items(username, password)
-    ril_url = RIL_TEMPLATE.gsub(/APIKEY/, ENV['RIL_API_KEY']).
-        gsub(/USERNAME/, username).
-        gsub(/PASSWORD/, password)
-
-    uri = URI.parse(ril_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    http.request(request)
+    data = { 'apikey' => ENV['RIL_API_KEY'] }
+    execute_ssl_post(RIL_GET, username, password, data)
   end
+
+  def check_inst_credentials(username, password)
+    execute_ssl_post(INST_AUTH, username, password, {}, true)
+  end
+
 
   # Items are passed in (via RIL) in the following format:
   # { 'list' =>
@@ -45,15 +47,50 @@ helpers do
 
   # Call the Instapaper API to create all the items
   def create_instapaper_items(username, password, item)
-    uri = URI.parse(INST_TEMPLATE)
+    item = { 'title' => item[:title], 'url' => item[:url] }
+    execute_ssl_post(INST_ADD, username, password, item, true)
+  end
+
+  def execute_ssl_post(url, username, password, data, basic_auth = false)
+    uri = URI.parse(url)
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
 
     request = Net::HTTP::Post.new(uri.request_uri)
-    request.basic_auth(username, password)
-    request.set_form_data({ 'title' => item[:title], 'url' => item[:url] })
+    if basic_auth
+      request.basic_auth(username, password)
+    else
+      data.merge({ 'username' => username, 'password' => password })
+    end
+    request.set_form_data(data)
     http.request(request)
+  end
+
+  def decode_ril_errors(code)
+    case code
+    when '400'
+      'Looks like we messed up - email us and let us know?'
+    when '401'
+      ERR_UNAUTHORIZED
+    when '403'
+      "Looks like we're too popular for our own good. Please wait a bit for us to go back under our rate limit."
+    when '503'
+      ERR_REMOTE
+    else
+      ERR_VAGUE
+    end
+  end
+
+  def decode_inst_errors(code)
+    case code
+    when '403'
+      ERR_UNAUTHORIZED
+    when '500'
+      ERR_REMOTE
+    else
+      ERR_VAGUE
+    end
   end
 end
 
@@ -62,16 +99,29 @@ get '/' do
 end
 
 post '/ready' do
-  raise 'Required credentials not supplied' unless params['ril'] and params['inst']
-  ril_items = fetch_readitlater_items(
+  @errors = {}
+
+  # Check and handle ReadItLater errors
+  ril_response = fetch_readitlater_items(
       params['ril']['username'], params['ril']['password'])
-  if ril_items.code == '200'
-    session['inst'] = params['inst']
-    json = JSON.parse(ril_items.body)
-    @items = convert_ril_json_to_instapaper(json)
-  else
-    @error = ril_items.code if ril_items.code != '200'
+  unless ril_response.code == '200'
+    @errors['ReadItLater'] = decode_ril_errors(ril_response.code)
   end
+
+  # Check for and handle Instapaper errors
+  inst_response = check_inst_credentials(
+      params['inst']['username'], params['inst']['username'])
+  if inst_response.code == '200'
+    session['inst'] = params['inst']
+  else
+    @errors['Instapaper'] = decode_inst_errors(inst_response.code)
+  end
+
+  if @errors.empty?
+    json = JSON.parse(ril_response.body)
+    @items = convert_ril_json_to_instapaper(json)
+  end
+
   haml :ready
 end
 
